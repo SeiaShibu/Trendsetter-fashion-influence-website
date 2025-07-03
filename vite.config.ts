a4bdf85726daf990fb8665e8edc58ef5f9075255
Seia Shibu <siyashibu.56@gmail.com>
2025-07-03 18:26:48 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/trendsetter/', // for GitHub Pages
   plugins: [react()],
+  optimizeDeps: { exclude: ['lucide-react'] },
   server: {
     proxy: {
       '/api': {
